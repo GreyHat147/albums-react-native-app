a01b504a8edc8915cb76bbb377c5113734273f5b
crypt0 <carlosandresrojasvalbuena521@gmail.com>
2018-07-29 19:40:15 -0500

    Se actualizo el albumDetail

--- src/components/AlgumList.js
+++ src/components/AlgumList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
-
+import AlbumDetail from '../components/AlbumDetail';
 class AlbumList extends Component {
     state = { albums: [] };
 
@@ -11,11 +11,17 @@ class AlbumList extends Component {
         .then(response => this.setState({ albums: response.data }));
     }
 
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album={album}/>
+        )
+    }
+
     render() {
         console.log(this.state);
         return (
             <View>
-                <Text>AlbumList !!!  </Text>
+                {this.renderAlbums()}
             </View>
         );
     }
