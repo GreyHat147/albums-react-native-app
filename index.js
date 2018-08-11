b5e8950b26904964fdbbab45a1d574320152a07e
crypt0 <carlosandresrojasvalbuena521@gmail.com>
2018-08-10 20:31:51 -0500

    Se crea diseño para el componente de card y se crea componente de boton reutilizable

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import AlbumList from './src/components/AlgumList';
 
 const App = () => {
     return (
-        <View>
+        <View style={{flex: 1}}>
             <Header headerText={'Albums'}/>
             <AlbumList />
         </View>
